016df4d0c40da940c2e86d86ffccb76e0dc5f1c0
Nikola Sarafimov <sarafimov.nikola12345@gmail.com>
2026-05-27 09:38:08 +0200

    Fix Vercel base path

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: mode === 'production' ? '/mk-dictionary-app/' : '/',
+  base: '/',
   plugins: [react()],
   server: {
     port: 8080,
