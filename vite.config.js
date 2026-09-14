6f656bd60cb0899f6346bb934905060b62e4f2a1
nikolasarafimov <sarafimov.nikola12345@gmail.com>
2026-09-14 16:09:10 +0200

    Optimize production database bundle

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,51 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import {
+  rmSync,
+} from 'node:fs'
+
+import {
+  resolve,
+} from 'node:path'
+
+import {
+  defineConfig,
+} from 'vite'
+
+import react
+  from '@vitejs/plugin-react'
+
+
+function excludeFullDatabaseFromBuild() {
+  return {
+    name: 'exclude-full-dictionary-database',
+
+    apply: 'build',
+
+    closeBundle() {
+      const fullDatabasePath =
+        resolve(
+          'dist',
+          'msd-mk.sqlite',
+        )
+
+      rmSync(
+        fullDatabasePath,
+        {
+          force: true,
+        },
+      )
+    },
+  }
+}
+
 
 export default defineConfig({
   base: '/',
-  plugins: [react()],
+
+  plugins: [
+    react(),
+    excludeFullDatabaseFromBuild(),
+  ],
+
   server: {
     port: 8080,
   },
